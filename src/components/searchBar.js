87a5312df22632f913104ff9b478677e625053f4
Alejandro <alejandro86mg@gmail.com>
2020-12-02 01:07:31 +0100

    styled the searchBar component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -22,14 +22,17 @@ class SearchBar extends Component {
         className="search-bar"
         onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
       >
-        <Field name="query" component={this.renderInput} />
+        <div className="search-bar__wrapper">
+          <Field name="query" component={this.renderInput} />
+          <p>Press return to search</p>
+        </div>
       </form>
     );
   }
 }
 
 SearchBar = reduxForm({
-  form: "searchBar",
+  form: "searchBar"
 })(SearchBar);
 
 SearchBar = withRouter(SearchBar);
